cd657c69fdb11d5973e2f9730f43ad117997ebd8
bdjos <42777251+bdjos@users.noreply.github.com>
2018-09-11 16:19:12 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sqlmodules',
+      version='0.1',
+      description='SQL modules for demand forecast',
+      url='http://github.com/bdjos/sqlmodules',
+      author='bdjos',
+      author_email='bdjos@gmail.com',
+      packages=['sqlmodules'])
